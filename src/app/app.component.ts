3b874cae9b47fd5e1dc7c1c3c681cd603ba56e95
martinezGaetan <martinezgaetan1@hotmail.fr>
2018-09-11 14:32:42 +0200

    Gérez la navigation avec le Routing - Naviguez avec le Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,39 +7,9 @@ import { AppareilService } from './services/appareil.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  isAuth = false;
-  appareils: any[];
   
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-
-  
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-  onEteindre() {
-    if(confirm('Etes-vous sûr de vouloir éteindre tous les appareils ?')) {
-      this.appareilService.switchOffAll();
-    } else {
-      return null;
+  constructor() {
     }
-  }
+    ngOnInit() {
+    }
 }
