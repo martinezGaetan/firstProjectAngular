fab6b28f38eb186c0683b32fe715da1c46ea6da2
martinezGaetan <martinezgaetan1@hotmail.fr>
2018-09-11 17:56:19 +0200

    Gérez la navigation avec le Routing - Paramètres de routes, en cours

--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,16 +1,19 @@
 export class AppareilService {
     appareils = [
         {
-          name: 'Machine à laver',
-          status: 'éteint'
+            id: 1,
+            name: 'Machine à laver',
+            status: 'éteint'
         },
         {
-          name: 'Frigo',
-          status: 'allumé'
+            id: 2,
+            name: 'Frigo',
+            status: 'allumé'
         },
         {
-          name: 'Ordinateur',
-          status: 'éteint'
+            id: 3,
+            name: 'Ordinateur',
+            status: 'éteint'
         }
       ];
 
@@ -33,5 +36,13 @@ export class AppareilService {
     switchOffOne(i: number) {
         this.appareils[i].status = 'éteint';
     }
+    getAppareilById(id: number) {
+        const appareil = this.appareils.find(
+          (s) => {
+            return s.id === id;
+          }
+        );
+        return appareil;
+      }
 
 }
